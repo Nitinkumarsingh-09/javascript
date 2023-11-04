657e69e0b7ce203e04f73b4e193396d8ceec7fd1
nitin kumar singh <nitinsingh39999@gmail.com>
2023-11-04 12:48:32 +0530

    this is date and time object tutorial

---
+++ date.js
@@ -0,0 +1,5 @@
+
+const myDate= new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+console.log(`date is ${myDate.getDate()} and months is ${myDate.getMonth()} and year is ${myDate.getFullYear()}`);
